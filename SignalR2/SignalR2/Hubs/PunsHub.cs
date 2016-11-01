475aae211f1c8c333b37b7d189d9e5a19960363d
PGS-SOFT\djarosch <djarosch@pgs-soft.com>
2016-11-01 15:07:27 +0100

    add working puns

---
+++ SignalR2/SignalR2/Hubs/PunsHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalR2.Hubs
+{
+    [HubName("puns")]
+    public class PunsHub : Hub<IPunsClientHandler>
+    {
+        public void SendPath(string path)
+        {
+            Clients.Others.DrawPath(path);
+        }
+    }
+
+    public interface IPunsClientHandler
+    {
+        void DrawPath(string path);
+    }
+}
